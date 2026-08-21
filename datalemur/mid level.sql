02efcd3753bad5ea2a896185af6dbafdb64ad660
LaxmiPitla <127838093+LaxmiPitla@users.noreply.github.com>
2026-08-21 17:06:26 +0530

    Update mid level.sql

--- datalemur/mid level.sql
+++ datalemur/mid level.sql
@@ -6,4 +6,18 @@ SELECT *,
 ROW_NUMBER() OVER(Partition by user_id order by transaction_date) as rn
 FROM transactions)t
 where rn = 3
-/*=====================================================*/
+/*==============================Second Highest Salary -FAANG SQL Interview Question=======================*/
+select 
+salary from (SELECT employee_id, salary 
+FROM employee
+order by salary desc
+limit 2)t
+order by salary asc limit 1;
+===================or======================================
+select 
+salary from 
+(select *,
+row_number() over(order by salary desc) as rn 
+from employee)t 
+where rn = 2;
+/*=========================================================================*/
